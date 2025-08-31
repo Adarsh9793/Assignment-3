ac429488a766fa22ded4a83e70b19dee5353c01e
Adarsh tripathi <adarshtripathi9793085@gmail.com>
2025-08-31 12:08:55 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a number:"))
+fact = 1
+if (n<0):
+    print("factorial  not exist")
+elif(n == 0):
+    print("factorial of 0 is 1")
+else:
+    for i in range(1,n+1):
+        fact *= i
+    print(f"Factorial of {n} is: {fact}")
+
---
+++ math module.py
@@ -0,0 +1,19 @@
+'''n = int(input("Enter a number:"))
+
+print("Square root: ",n**0.5)
+print("Logarithm:",n**0.1)
+print("Sine:",n**0.5)
+'''
+import math
+
+
+num = float(input("Enter a number: "))
+sqrt = math.sqrt(num)
+log = math.log(num)
+sine = math.sin(num)
+
+
+
+print(f"Square root: {sqrt}")
+print(f"logarithm : {log}")
+print(f" Sine : {sine}")
